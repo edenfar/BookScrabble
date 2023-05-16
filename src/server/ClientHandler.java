5d1256dd028915abd27309d6ecb0d8a622354072
Eden Fargion <eden@DESKTOP-9LH03VJ>
2023-05-16 19:39:01 +0300

    Add server classes

--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -4,7 +4,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 interface ClientHandler {
-    void handleClient(InputStream inFromclient, OutputStream outToClient);
+    void handleClient(InputStream inFromClient, OutputStream outToClient);
 
     void close();
 }
