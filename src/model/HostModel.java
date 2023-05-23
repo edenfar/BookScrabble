60250fc2494311a5ae13e96aee8c0f346b910053
Eden Fargion <eden@DESKTOP-9LH03VJ>
2023-05-23 18:40:37 +0300

    Add model classes

---
+++ src/model/HostModel.java
@@ -0,0 +1,11 @@
+package model;
+
+public class HostModel extends Model {
+    public void createGame(String playerName, String[] fileNames) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void startGame() {
+        throw new UnsupportedOperationException();
+    }
+}
