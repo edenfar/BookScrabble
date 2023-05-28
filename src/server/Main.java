7f02eb2a5620525e7daf9a2648b2774bce622bae
Eden Fargion <eden@DESKTOP-9LH03VJ>
2023-05-28 19:01:40 +0300

    Make Main more readable

--- src/server/Main.java
+++ src/server/Main.java
@@ -2,7 +2,6 @@ package server;
 
 import java.util.Scanner;
 
-
 public class Main {
 
     public static final int PORT = 6123;
@@ -14,24 +13,12 @@ public class Main {
         MyServer server = new MyServer(PORT, new PlayerHandler(), 3);
         server.start();
 
-
         while (!input.equals("stop")) {
             input = scanner.nextLine();
-
-            if (input.equalsIgnoreCase("stop")) {
-                System.out.println("Stopping the main program...");
-
-                // Perform any necessary cleanup or shutdown tasks
-                server.close();
-
-                // Close resources and exit the program
-                scanner.close();
-                System.exit(0);
-            }
         }
 
-
-
-
+        System.out.println("Stopping the main program...");
+        server.close();
+        scanner.close();
     }
 }
