3d8331d63b030ea46beb39b8ad7678519b0e5e93
ShiraSilverman <shirabuskila@gmail.com>
2023-06-04 20:24:35 +0300

    starting to work on the board

---
+++ test/java/Main.java
@@ -0,0 +1,21 @@
+// import server.Game;
+// import server.GamesManager;
+// import java.util.HashMap;
+// import java.util.Map;
+// public class Main {
+
+
+//     public static void main(String[] args) {
+//         // Test the getRandomName() method
+//         GamesManager gamesManager = new GamesManager();
+//         gamesManager.games = Map.of(
+//                 "game1", new Game("game1", "game1.txt", Player host, int rounds),
+//                 "game2", new Game(),
+//                 "game3", new Game()
+//         );
+
+//         String randomName = gamesManager.getRandomName();
+//         System.out.println("Random name: " + randomName);
+//     }
+// }
+
